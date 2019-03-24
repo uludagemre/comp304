cbf531245b6b0be00137d590dd20686c66101ed1
emre.uludag.koc <uludag.emre.tr@gmail.com>
2019-03-25 00:33:15 +0300

    Listing history elements and running them works fine

--- Assignment2/shelldon.c
+++ Assignment2/shelldon.c
@@ -15,133 +15,225 @@ KUSIS ID: 31760 PARTNER NAME: Arda Arslan
 #include <time.h>
 #include <wait.h>
 
+#define MAX_LINE 80 /* 80 chars per line, per command, should be enough. */
 
-#define MAX_LINE       80 /* 80 chars per line, per command, should be enough. */
-
-int parseCommand(char inputBuffer[], char *args[],int *background, int *shouldRedirect, int *shouldAppend);
+int parseCommand(char inputBuffer[], char *args[], int *background, int *shouldRedirect, int *shouldAppend, int isInHistory, char historyCommand[]);
 
 int main(void)
 {
-  char inputBuffer[MAX_LINE]; 	        /* buffer to hold the command entered */
-  int background;             	        /* equals 1 if a command is followed by '&' */
-  char *args[MAX_LINE/2 + 1];	        /* command line (of 80) has max of 40 arguments */
-  pid_t child;            		/* process id of the child process */
-  int status;           		/* result from execv system call*/
+  char inputBuffer[MAX_LINE];   /* buffer to hold the command entered */
+  int background;               /* equals 1 if a command is followed by '&' */
+  char *args[MAX_LINE / 2 + 1]; /* command line (of 80) has max of 40 arguments */
+  pid_t child;                  /* process id of the child process */
+  int status;                   /* result from execv system call*/
   int shouldrun = 1;
   int shouldRedirect;
   int shouldAppend;
   FILE *mystdout = stdout;
-  char* history[1000];
+  char history[100][50];
   int history_count = 0;
-	
+  char historyCommand[MAX_LINE];
+  int isInHistory;
+
   int i, upper;
   int first_time = 1;
   int first_time_2 = 1;
-		
-  while (shouldrun){            		/* Program terminates normally inside setup */
+
+  while (shouldrun)
+  { /* Program terminates normally inside setup */
+
     background = 0;
     shouldRedirect = 0;
     shouldAppend = 0;
     first_time_2 = 1;
 
-    if (first_time != 1) {
+    if (first_time != 1)
+    {
       fclose(stdout);
     }
-    else {
+    else
+    {
       first_time = 1;
     }
     stdout = mystdout;
-		
-    shouldrun = parseCommand(inputBuffer,args,&background,&shouldRedirect,&shouldAppend);       /* get next command */
+
+    shouldrun = parseCommand(inputBuffer, args, &background, &shouldRedirect, &shouldAppend, isInHistory, historyCommand); /* get next command */
 
     char **ptr = args;
     char current_command[MAX_LINE];
     strcpy(current_command, "");
-    while (*ptr != NULL) {
-      if (first_time_2 != 1) {
+    while (*ptr != NULL)
+    {
+      if (first_time_2 != 1)
+      {
         strcat(current_command, " ");
         strcat(current_command, *ptr);
       }
-      else {
+      else
+      {
         strcpy(current_command, *ptr);
         first_time_2 = 0;
       }
       *ptr++;
     }
-    if (background == 1) {
+    if (background == 1)
+    {
       strcat(current_command, " &");
     }
-    printf("%s\n", current_command);
-
-    // history[history_count] = current_command;
-
-    // char **sss = history;
-    // while (*sss != NULL) {
-      
+    strcat(current_command, "\0");
+    if (args[0][0] != '!')
+    {
+      strcpy(history[history_count], current_command);
+      history_count++;
+    }
     // }
 
     if (strncmp(inputBuffer, "exit", 4) == 0)
-      shouldrun = 0;     /* Exiting from shelldon*/
+      shouldrun = 0; /* Exiting from shelldon*/
 
     //Below is the answer of the first part
 
-    if (shouldrun) {
-        child = fork();
-        if (child == 0) { // child
-            char path[500];
-            char filename[500];
-            
-            int count = 0;
+    if (shouldrun)
+    {
+      child = fork();
+      if (child == 0)
+      { // child
+        char path[500];
+        char filename[500];
 
-            if ((shouldRedirect == 1) && (shouldAppend == 1)) {
-              while (true) {
-                if (strncmp(args[count], ">>", 2) == 0) {
-                  strcpy(filename, args[count+1]);
-                  args[count] = NULL;
-                  break;
-                }
-                count++;
+        int count = 0;
+
+        if ((shouldRedirect == 1) && (shouldAppend == 1))
+        {
+          while (true)
+          {
+            if (strncmp(args[count], ">>", 2) == 0)
+            {
+              strcpy(filename, args[count + 1]);
+              args[count] = NULL;
+              break;
+            }
+            count++;
+          }
+
+          freopen(filename, "a+", stdout);
+        }
+        if ((shouldRedirect == 1) && (shouldAppend == 0))
+        {
+          while (true)
+          {
+            if (strncmp(args[count], ">", 1) == 0)
+            {
+              strcpy(filename, args[count + 1]);
+              args[count] = NULL;
+              break;
+            }
+            count++;
+          }
+          freopen(filename, "w+", stdout);
+        }
+
+        if (strcmp(args[0], "gcc") == 0)
+        {
+          strcpy(path, "/usr/bin/");
+        }
+        else if (strcmp(args[0], "cd") == 0)
+        {
+          exit(0);
+        }
+
+        else if (args[0][0] == '!')
+        {
+
+          if (args[0][1] != '!')
+          {
+
+            int length = strlen(args[0]);
+            char subbuff[5];
+            memcpy(subbuff, &args[0][1], length);
+            subbuff[length] = '\0';
+            int targetHistory = atoi(subbuff);
+            strcpy(historyCommand, history[targetHistory - 1]);
+            isInHistory = 1;
+            int lengthOfHistory = strlen(historyCommand);
+            historyCommand[lengthOfHistory + 1] = '\n';
+            strcat(historyCommand, "\n\0");
+            char path[10];
+            shouldrun = parseCommand(inputBuffer, args, &background, &shouldRedirect, &shouldAppend, isInHistory, historyCommand);
+            if (strcmp(args[0], "history") == 0)
+            {
+
+              if (history_count == 1)
+              {
+                printf("No history data found \n");
+                break;
               }
-              
-              freopen(filename,"a+",stdout);
-              
-            }
-            if ((shouldRedirect == 1) && (shouldAppend == 0)) {
-              while (true) {
-                if (strncmp(args[count], ">", 1) == 0) {
-                  strcpy(filename, args[count+1]);
-                  args[count] = NULL;
-                  break;
-                }
-                count++;
+
+              int historyEndingIndex = history_count - 1;
+              int index = history_count;
+              printf("\nLast commands: \n");
+              for (int i = historyEndingIndex; i >= 0; i--) //print at most last 10 elements in the histroy
+              {
+                printf("%d %s\n", index, history[i]);
+                index--;
               }
-              freopen(filename,"w+",stdout);
-            }
-
-            if (strcmp(args[0], "gcc") == 0) {
-              strcpy(path, "/usr/bin/");
-            }
-            else if (strcmp(args[0], "cd") == 0) {
-              exit(0);
-            }
-            else {
-              strcpy(path, "/bin/");
-            }
-
+            }else{
+            strcpy(path, "/bin/");
             strcat(path, args[0]);
+            printf("this is path %s", path);
             execv(path, args);
-            exit(0);
-        }
-        else { // parent
-            if (background == 1) {
-                //This is the part when user puts '&' sign after the first command    
-                wait(NULL);
             }
+            
+          }
+          else
+          {
+            int targetHistory = history_count - 1;
+            strcpy(historyCommand, history[targetHistory - 1]);
+            isInHistory = 1;
+            continue;
+          }
+        }
+        else if (strcmp(args[0], "history") == 0)
+        {
 
-            if (strcmp(args[0], "cd") == 0) {
-              chdir(args[1]);
-            }
+          if (history_count == 1)
+          {
+            printf("No history data found \n");
+            break;
+          }
+
+          int historyEndingIndex = history_count - 1;
+          int index = history_count;
+          printf("\nLast commands: \n");
+          for (int i = historyEndingIndex; i >= 0; i--) //print at most last 10 elements in the histroy
+          {
+            printf("%d %s\n", index, history[i]);
+            index--;
+          }
         }
+        else
+        {
+          strcpy(path, "/bin/");
+        }
+
+        strcat(path, args[0]);
+        execv(path, args);
+
+        exit(0);
+      }
+      else
+      { // parent
+        if (background == 1)
+        {
+          //This is the part when user puts '&' sign after the first command
+          wait(NULL);
+        }
+
+        if (strcmp(args[0], "cd") == 0)
+        {
+          chdir(args[1]);
+        }
+      }
 
       /*
 	After reading user input, the steps are 
@@ -161,111 +253,128 @@ int main(void)
  * will become null-terminated, C-style strings. 
  */
 
-int parseCommand(char inputBuffer[], char *args[],int *background, int *shouldRedirect, int *shouldAppend)
+int parseCommand(char inputBuffer[], char *args[], int *background, int *shouldRedirect, int *shouldAppend, int isInHistory, char historyCommand[])
 {
-    int length,		/* # of characters in the command line */
-      i,		/* loop index for accessing inputBuffer array */
-      start,		/* index where beginning of next command parameter is */
-      ct,	        /* index of where to place the next parameter into args[] */
-      command_number;	/* index of requested command number */
-    
-    ct = 0;
-	
-    /* read what the user enters on the command line */
-    do {
-	  printf("shelldon>");
-	  fflush(stdout);
-	  length = read(STDIN_FILENO,inputBuffer,MAX_LINE); 
-    }
-    while (inputBuffer[0] == '\n'); /* swallow newline characters */
-	
-    /**
+  int length,         /* # of characters in the command line */
+      i,              /* loop index for accessing inputBuffer array */
+      start,          /* index where beginning of next command parameter is */
+      ct,             /* index of where to place the next parameter into args[] */
+      command_number; /* index of requested command number */
+
+  ct = 0;
+
+  /* read what the user enters on the command line */
+
+  if (isInHistory == 0)
+  {
+    do
+    {
+      printf("shelldon>");
+      fflush(stdout);
+      length = read(STDIN_FILENO, inputBuffer, MAX_LINE);
+    } while (inputBuffer[0] == '\n'); /* swallow newline characters */
+  }
+  else
+  {
+
+    strcpy(inputBuffer, historyCommand);
+    int tempLength = strlen(inputBuffer);
+    length = strlen(inputBuffer);
+    isInHistory = 0;
+
+    //You must drop the thing down
+  }
+
+  /**
      *  0 is the system predefined file descriptor for stdin (standard input),
      *  which is the user's screen in this case. inputBuffer by itself is the
      *  same as &inputBuffer[0], i.e. the starting address of where to store
      *  the command that is read, and length holds the number of characters
      *  read in. inputBuffer is not a null terminated C-string. 
-     */    
-    start = -1;
-    if (length == 0)
-      exit(0);            /* ^d was entered, end of user command stream */
-    
-    /** 
+     */
+  start = -1;
+  if (length == 0)
+    exit(0); /* ^d was entered, end of user command stream */
+
+  /** 
      * the <control><d> signal interrupted the read system call 
      * if the process is in the read() system call, read returns -1
      * However, if this occurs, errno is set to EINTR. We can check this  value
      * and disregard the -1 value 
      */
 
-    if ( (length < 0) && (errno != EINTR) ) {
-      perror("error reading the command");
-      exit(-1);           /* terminate with error code of -1 */
-    }
-    
-    /**
+  if ((length < 0) && (errno != EINTR))
+  {
+    perror("error reading the command");
+    exit(-1); /* terminate with error code of -1 */
+  }
+
+  /**
      * Parse the contents of inputBuffer
      */
-    
-    for (i=0;i<length;i++) { 
-      /* examine every character in the inputBuffer */
-      
-      switch (inputBuffer[i]){
-      case ' ':
-      case '\t' :               /* argument separators */
-	if(start != -1){
-	  args[ct] = &inputBuffer[start];    /* set up pointer */
-	  ct++;
-	}
-	inputBuffer[i] = '\0'; /* add a null char; make a C string */
-	start = -1;
-	break;
-	
-      case '\n':                 /* should be the final char examined */
-	if (start != -1){
-	  args[ct] = &inputBuffer[start];     
-	  ct++;
-	}
-	inputBuffer[i] = '\0';
-	args[ct] = NULL; /* no more arguments to this command */
-	break;
-	
-      default :             /* some other character */
-	if (start == -1)
-	  start = i;
-	if (inputBuffer[i] == '&') {
-	  *background  = 1;
-	  inputBuffer[i-1] = '\0';
-	}
-      
-  if ((i != 0) && (inputBuffer[i-1] != '>') && (inputBuffer[i] == '>') && (inputBuffer[i+1] != '>'))  {
-    *shouldAppend = 0;
-    *shouldRedirect = 1;
-    
-    
-  }
-  if ((i != 0) && (inputBuffer[i-1] != '>') && (inputBuffer[i] == '>') && (inputBuffer[i+1] == '>'))  {
-    *shouldAppend = 1;
-    *shouldRedirect = 1;
-    
-  }
-  // if (strncmp(inputBuffer[i], ">>")){ 
-  //   *shouldAppend = 0;
-  //   *shouldRedirect = 1;
-  // }
-      
-      
-      } /* end of switch */
-    }    /* end of for */
-  
-    /**
+
+  for (i = 0; i < length; i++)
+  {
+    /* examine every character in the inputBuffer */
+
+    switch (inputBuffer[i])
+    {
+    case ' ':
+    case '\t': /* argument separators */
+      if (start != -1)
+      {
+        args[ct] = &inputBuffer[start]; /* set up pointer */
+        ct++;
+      }
+      inputBuffer[i] = '\0'; /* add a null char; make a C string */
+      start = -1;
+      break;
+
+    case '\n': /* should be the final char examined */
+      if (start != -1)
+      {
+        args[ct] = &inputBuffer[start];
+        ct++;
+      }
+      inputBuffer[i] = '\0';
+      args[ct] = NULL; /* no more arguments to this command */
+      break;
+
+    default: /* some other character */
+      if (start == -1)
+        start = i;
+      if (inputBuffer[i] == '&')
+      {
+        *background = 1;
+        inputBuffer[i - 1] = '\0';
+      }
+
+      if ((i != 0) && (inputBuffer[i - 1] != '>') && (inputBuffer[i] == '>') && (inputBuffer[i + 1] != '>'))
+      {
+        *shouldAppend = 0;
+        *shouldRedirect = 1;
+      }
+      if ((i != 0) && (inputBuffer[i - 1] != '>') && (inputBuffer[i] == '>') && (inputBuffer[i + 1] == '>'))
+      {
+        *shouldAppend = 1;
+        *shouldRedirect = 1;
+      }
+      // if (strncmp(inputBuffer[i], ">>")){
+      //   *shouldAppend = 0;
+      //   *shouldRedirect = 1;
+      // }
+
+    } /* end of switch */
+  }   /* end of for */
+
+  /**
      * If we get &, don't enter it in the args array
      */
-    
-    if (*background)
-      args[--ct] = NULL;
-    
-    args[ct] = NULL; /* just in case the input line was > 80 */
-    
-    return 1;
-    
+
+  if (*background)
+    args[--ct] = NULL;
+  args[ct] = NULL; /* just in case the input line was > 80 */
+  // strcpy(inputBuffer,"");
+  return 1;
+
 } /* end of parseCommand routine */
