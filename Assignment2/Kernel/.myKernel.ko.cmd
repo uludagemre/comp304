3962759b8e8bcbf5ff6eab8efcaf8bd3917ed142
emre.uludag.koc <uludag.emre.tr@gmail.com>
2019-03-29 23:00:29 +0300

    Cleaned up kernel I will write the report tonight and will continue working on Pacman assignment my Pacman assignment

--- Assignment2/Kernel/.myKernel.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/user/Desktop/comp304/Assignment2/Kernel/myKernel.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/user/Desktop/comp304/Assignment2/Kernel/myKernel.ko /home/user/Desktop/comp304/Assignment2/Kernel/myKernel.o /home/user/Desktop/comp304/Assignment2/Kernel/myKernel.mod.o ;  true
